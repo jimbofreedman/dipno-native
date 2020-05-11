436707d0aff199f881b87dfb902a86f70a4deeb1
Jimbo Freedman <jimbo@jimbo.rocks>
2020-05-10 21:24:32 -0600

    Display list of users

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { Text, Container, Content, List, ListItem } from 'native-base';
 import { observer } from 'mobx-react';
+import { toJS } from 'mobx';
 
 import useStores from '../useStores';
 
@@ -8,7 +9,7 @@ function HomeScreen() {
   const { userStore } = useStores();
 
   React.useEffect(() => {
-    if (!userStore.loading && !userStore.loaded) {
+    if (!userStore.loading && !userStore.error && !userStore.all().length) {
       userStore.loadAll();
     }
   });
@@ -26,9 +27,10 @@ function HomeScreen() {
   return (
     <Container>
       <List>
-        {userStore.all().map(u => (
-          <ListItem>{u.attributes.firstName}</ListItem>
-        ))}
+        {userStore.all().map(u => {
+          console.log(toJS(u.attributes));
+          return <ListItem key={u.attributes.first_name}><Text>{u.attributes.first_name} {u.attributes.last_name}</Text></ListItem>;
+        })}
       </List>
     </Container>
   );
